7236c818a1a5dba1e75ef3f77ea26b15e3a2aba5
Nicolas Bouilhol <nicolas@bouilhol.com>
2011-01-17 06:05:05 -0800

    postlink + confirm-dialog issues solved

--- js/jquery.postlink.js
+++ js/jquery.postlink.js
@@ -1,24 +1,24 @@
-﻿(function ($) {
+(function ($) {
     $.fn.postlink = function (options) {
         var opts = $.extend({}, $.fn.postlink.defaults, options);
         if (opts.hasConfirmDialog == true) {
-            $(this).confirmdialog({ onGetUrl: function () { return this.attr("href") }, onYes: postForm });
+            this.confirmdialog({ onGetUrl: function () { return this.attr("href") }, onYes: function (url) { postForm(url) } });
         }
         else {
-            $(this).live("click", function (event) {
+            this.live("click", function (event) {
                 event.preventDefault();
-                postForm.call($(this));
+                postForm(this.attr("href"));
             });
         }
     }
-    function postForm() {
-        var linkForm = createPostForm.call(this, this.attr("href"));
+    function postForm(url) {
+        var linkForm = createPostForm.call(this, url);
         $('body').append(linkForm);
         linkForm.submit();
     }
-    function createPostForm(href) {
+    function createPostForm(url) {
         var linkForm = $('<form></form>')
-                .attr("action", href)
+                .attr("action", url)
                 .attr("method", "post")
                 .attr("id", "postlinkForm_" + new Date().getTime());
         return linkForm;
